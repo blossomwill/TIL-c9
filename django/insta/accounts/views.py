bbb7015897cb67077e098a5d8b1f35af47d3b6f4
Minsu Seo <tjalstn10@gmail.com>
2019-04-11 08:47:28 +0000

    190411 | Insta - Comment, Like, People

--- django/insta/accounts/views.py
+++ django/insta/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
 # Create your views here.
 def signup(request):
     if request.user.is_authenticated:
@@ -34,6 +35,12 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('posts:list')
+    
+def people(request, username):
+    # get_user_model() #=> User
+    people = get_object_or_404(get_user_model(), username=username) 
+    # column의 username = 주소로부터 넘겨받은 username
+    return render(request, 'accounts/people.html', {'people': people})
     
     
     
--- django/insta/posts/views.py
+++ django/insta/posts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
-from .models import Post
+from django.views.decorators.http import require_POST, require_http_methods
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 # Create your views here.
 
 def list(request):
     posts = Post.objects.order_by('-id').all()
-    return render(request, 'posts/list.html', {'posts': posts})
+    comment_form = CommentForm()
+    return render(request, 'posts/list.html', {'posts': posts, 'comment_form': comment_form})
     
 @login_required
 def create(request):
@@ -20,7 +22,9 @@ def create(request):
     else:
         post_form = PostForm()
     return render(request, 'posts/form.html', {'post_form':post_form})
-    
+
+
+@login_required  
 def update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if post.user != request.user:
@@ -34,7 +38,7 @@ def update(request, post_id):
         post_form = PostForm(instance=post)
         return render(request, 'posts/form.html', {'post_form':post_form})
     
-    
+@login_required     
 def delete(request, post_id):
     # post = Post.objects.get(id=post_id)
     post = get_object_or_404(Post, id=post_id)
@@ -43,4 +47,37 @@ def delete(request, post_id):
         
     post.delete()
     return redirect('posts:list')    
+
+@login_required    
+@require_POST
+def comment_create(request, post_id):
+    # post = Post.objects.get(id=post_id)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.user = request.user
+        comment.post_id = post_id
+        comment.save()
+    return redirect('posts:list')
     
+@require_http_methods(['GET', 'POST'])
+def comment_delete(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.user != request.user:
+        return redirect('posts:list')
+        
+    comment.delete()
+    return redirect('posts:list')
+    
+@login_required
+def like(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    # 좋아요를 누른 사람중에 자기자신이 있으면 취소를 해서 없애버림
+    if request.user in post.like_users.all():
+          # 2. 좋아요 취소
+        post.like_users.remove()
+    # 1. 좋아요 !
+    # 좋아요를 누른 사람에 현재 유저를 추가
+    else:
+        post.like_users.add(request.user) # post를 좋아요 누른 user들, 현재 유저가 좋아요 누르는 유저
+    return redirect('posts:list')
